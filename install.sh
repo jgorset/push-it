4def47312c7ccbc3370303589774e9e49332416d
Johannes Gorset <jgorset@gmail.com>
2013-10-31 15:58:21 +0100

    Report on progress

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Downloading song..."
 curl "http://forrykt.com/push-it/push-it.m4a" > ~/Music/push-it.m4a
+
+echo "Applying commit-msg hook..."
 curl "http://forrykt.com/push-it/commit-msg" > .git/hooks/commit-msg
 chmod +x .git/hooks/commit-msg
+
+echo "Done."
