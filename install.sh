d82fc111c74a6138680ad0720cee429d322c90ae
Johannes Gorset <jgorset@gmail.com>
2014-10-27 16:52:26 +0100

    Host it on GitHub

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Downloading song..."
-curl -s "http://forrykt.com/push-it/push-it.mp3" > ~/Music/push-it.mp3
+curl -s --location "https://raw.github.com/jgorset/push-it/master/push-it.mp3" > ~/Music/push-it.mp3
 
 echo "Applying commit-msg hook..."
-curl -s "http://forrykt.com/push-it/commit-msg" > .git/hooks/commit-msg
+curl -s --location "https://raw.github.com/jgorset/push-it/master/commit-msg" > .git/hooks/commit-msg
 chmod +x .git/hooks/commit-msg
 
 echo "Done."
