891a597dc2874a79b9a38afd9519a2b9a07b8202
Johannes Gorset <jgorset@gmail.com>
2014-10-27 16:45:19 +0100

    Trim the song

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading song..."
-curl "http://forrykt.com/push-it/push-it.m4a" > ~/Music/push-it.m4a
+curl "http://forrykt.com/push-it/push-it.mp3" > ~/Music/push-it.mp3
 
 echo "Applying commit-msg hook..."
 curl "http://forrykt.com/push-it/commit-msg" > .git/hooks/commit-msg
