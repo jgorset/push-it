5a259dc1d5370d10cde6051b92dc8eb73c6f8c1e
Johannes Gorset <jgorset@gmail.com>
2015-09-10 19:57:42 +0200

    Add ASCII art

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+echo "
+      ___  __  ________ __    __________
+     / _ \/ / / / __/ // /   /  _/_  __/
+    / ___/ /_/ /\ \/ _  /   _/ /  / /
+   /_/   \____/___/_//_/   /___/ /_/
+"
+
 echo "Downloading songs..."
-curl -s --location "https://raw.github.com/jgorset/push-it/master/push-it.mp3" > ~/Music/push-it.mp3
-curl -s --location "https://raw.github.com/jgorset/push-it/master/push-it-to-the-limit.mp3" > ~/Music/push-it-to-the-limit.mp3
+curl --progress-bar --location "https://raw.github.com/jgorset/push-it/master/push-it.mp3" > ~/Music/push-it.mp3
+curl --progress-bar --location "https://raw.github.com/jgorset/push-it/master/push-it-to-the-limit.mp3" > ~/Music/push-it-to-the-limit.mp3
+echo ""
 
 echo "Applying commit-msg hook..."
 curl -s --location "https://raw.github.com/jgorset/push-it/master/commit-msg" > .git/hooks/commit-msg
